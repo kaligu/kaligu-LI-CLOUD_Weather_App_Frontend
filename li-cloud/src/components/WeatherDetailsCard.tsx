95fabea94f6298585a558b2dc387248c8d36379b
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-02-20 17:53:19 +0530

    added WeatherDetailsCard to props

--- li-cloud/src/components/WeatherDetailsCard.tsx
+++ li-cloud/src/components/WeatherDetailsCard.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-function WeatherDetailsCard() {
+interface WeatherDetails {
+    description: string;
+    feelsLike: string;
+    minTemperature: string;
+    maxTemperature: string;
+    windSpeed: string;
+    cloudiness: string;
+    pressure: string;
+    humidity: string;
+    seaLevelPressure: string;
+    groundLevelPressure: string;
+}
+
+function WeatherDetailsCard(props: WeatherDetails) {
   return (
     <>
      <div className='flex flex-col justify-center items-start ml-3'>
-        <h1 className='mt-2'>📙Description : Few Clouds</h1>
-        <h1>🔥Feels Like: 308.66K</h1>
-        <h1>🕒Minimum Temperature: 304.46K</h1>
-        <h1>🕘Maximum Temperature:: 304.46K</h1>
-        <h1>🍃 Wind Speed: 4.2m/s</h1>
-        <h1>☁️Cloudiness: 23%</h1>
-        <h1>⏏Pressure: 1008hPa</h1>
-        <h1>💦Humidity: 60%</h1>
-        <h1>🌊Sea Level Pressure: 1008hPa </h1>
-        <h1>🕳️Ground Level Pressure: 1008hPa </h1>
+        <h1 className='mt-2'>📙Description : {props.description}</h1>
+        <h1>🔥Feels Like: {props.feelsLike}</h1>
+        <h1>🕒Minimum Temperature: {props.minTemperature}</h1>
+        <h1>🕘Maximum Temperature: {props.maxTemperature}</h1>
+        <h1>🍃 Wind Speed: {props.windSpeed}</h1>
+        <h1>☁️Cloudiness: {props.cloudiness}</h1>
+        <h1>⏏Pressure: {props.pressure}</h1>
+        <h1>💦Humidity: {props.humidity}</h1>
+        <h1>🌊Sea Level Pressure: {props.seaLevelPressure}</h1>
+        <h1>🕳️Ground Level Pressure: {props.groundLevelPressure}</h1>
      </div>
     </>
   )
 }
 
-export default WeatherDetailsCard
+export default WeatherDetailsCard;
